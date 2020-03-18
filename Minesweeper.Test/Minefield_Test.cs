42d09213083d24eebe25cbf4f4377d5da9351422
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-03-18 16:52:50 -0400

    updated testing to match some of the previous refactoring

--- Minesweeper.Test/Minefield_Test.cs
+++ Minesweeper.Test/Minefield_Test.cs
@@ -7,69 +7,34 @@ namespace Minesweeper.Test
     public class Minefield_Test
     {
         [Theory]
-        [InlineData(1, 1)]
-        [InlineData(10, 10)]
-        [InlineData(7, 10)]
-        [InlineData(100, 12)]
-        public void MinefieldConstructor_PositiveValues_noErrors(int cols, int rows)
+        [InlineData(1, 2, 1)]
+        [InlineData(2, 1, 1)]
+        [InlineData(10, 10, 1)]
+        [InlineData(7, 10, 1)]
+        [InlineData(100, 12, 1)]
+        public void MinefieldConstructor_PositiveValues_noErrors(int cols, int rows, int numOfMines)
         {
-            Minefield minefield = new Minefield(cols, rows);
+            Minefield minefield = new Minefield(cols, rows, numOfMines);
 
             Assert.Equal(cols, minefield.Cols);
             Assert.Equal(rows, minefield.Rows);
-            Assert.Equal(cols * rows, minefield.Mines.Length);
-
-            foreach (var mine in minefield.Mines)
-            {
-                Assert.False(mine);
-            }
-
+            Assert.Equal(cols * rows, minefield.Plots.Length);
         }
 
         [Theory]
-        [InlineData(0, 1)]
-        [InlineData(1, 0)]
-        [InlineData(0, 0)]
-        [InlineData(-10, -10)]
-        public void MinefieldConstructor_InvalidRowsAndCols_ThrowsException(int cols, int rows)
+        [InlineData(0, 1, 1)]
+        [InlineData(1, 0, 1)]
+        [InlineData(0, 0, 1)]
+        [InlineData(-10, -10, 1)]
+        [InlineData(7, 10, -5)]
+        [InlineData(100, 12, 0)]
+        [InlineData(1, 1, 10)]
+        public void MinefieldConstructor_InvalidRowsAndCols_ThrowsException(int cols, int rows, int numOfMines)
         {
-            Assert.Throws<ArgumentException>(() => new Minefield(cols, rows));
+            Assert.Throws<ArgumentException>(() => new Minefield(cols, rows, numOfMines));
         }
 
-        [Theory]
-        [InlineData(1, 1, 1)]
-        [InlineData(10, 10, 100)]
-        [InlineData(7, 10, 25)]
-        [InlineData(100, 12, 120)]
-        public void PlaceMines_ValidNumberOfMines_noErrors(int cols, int rows, int numOfMines)
-        {
-            Minefield minefield = new Minefield(cols, rows);
 
-            minefield.PlaceMines(numOfMines);
-
-            int countOfMines = 0;
-
-            foreach (var mine in minefield.Mines)
-            {
-                if (mine)
-                {
-                    countOfMines++;
-                }
-            }
-
-            Assert.Equal(numOfMines, countOfMines);
-        }
-
-        [Theory]
-        [InlineData(1, 1, -1)]
-        [InlineData(10, 10, 1000)]
-        [InlineData(7, 10, -25)]
-        [InlineData(100, 12, 12000)]
-        public void PlaceMines_InvalidNumberOfMines_ThrowsException(int cols, int rows, int numOfMines)
-        {
-            var minefield = new Minefield(cols, rows);
-
-            Assert.Throws<ArgumentException>(() => minefield.PlaceMines(numOfMines));
-        }
     }
+
 }
